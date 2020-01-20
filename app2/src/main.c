a7ba12c938367f3d4f69ef7fa2e3c44a8c204125
zjjliujs <zjj_ljs@163.com>
2020-01-20 15:11:14 +0800

    app2 source code format

--- app2/src/main.c
+++ app2/src/main.c
@@ -2,8 +2,6 @@
 
 #include "exampleapp.h"
 
-int
-main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]) {
   return g_application_run (G_APPLICATION (example_app_new ()), argc, argv);
 }
